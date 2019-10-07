a51672d5559d5618cd078a9211e0b10705afe6ce
Andrew Steinmetz <andrew@plusmobilapps.com>
2019-10-07 01:30:58 -0700

    shared element transition for android guy works with primitive bottom nav hiding and showing

--- app/src/main/java/com/plusmobileapps/bottomnavtransition/ui/DashboardDetailFragment.kt
+++ app/src/main/java/com/plusmobileapps/bottomnavtransition/ui/DashboardDetailFragment.kt
@@ -1,20 +1,61 @@
 package com.plusmobileapps.bottomnavtransition.ui
 
+import android.graphics.drawable.Drawable
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import androidx.fragment.app.Fragment
+import androidx.transition.TransitionInflater
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.DataSource
+import com.bumptech.glide.load.engine.GlideException
+import com.bumptech.glide.request.RequestListener
+import com.bumptech.glide.request.target.Target
 import com.plusmobileapps.bottomnavtransition.R
 
-class DashboardDetailFragment :  Fragment() {
+class DashboardDetailFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        sharedElementEnterTransition = TransitionInflater.from(requireContext()).inflateTransition(android.R.transition.move)
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        postponeEnterTransition()
         val root = inflater.inflate(R.layout.fragment_dashboard_detail, container, false)
+        root.findViewById<ImageView>(R.id.dashboard_detail_imageview)?.let { imageView ->
+            Glide.with(this)
+                .load(ANDROID_JETPACK_IMAGE_URL)
+                .listener(object : RequestListener<Drawable> {
+                    override fun onLoadFailed(
+                        e: GlideException?,
+                        model: Any?,
+                        target: Target<Drawable>?,
+                        isFirstResource: Boolean
+                    ): Boolean {
+                        startPostponedEnterTransition()
+                        return false
+                    }
+
+                    override fun onResourceReady(
+                        resource: Drawable?,
+                        model: Any?,
+                        target: Target<Drawable>?,
+                        dataSource: DataSource?,
+                        isFirstResource: Boolean
+                    ): Boolean {
+                        startPostponedEnterTransition()
+                        return false
+                    }
+                })
+                .into(imageView)
+        }
         return root
     }
 
--- app/src/main/java/com/plusmobileapps/bottomnavtransition/ui/DashboardFragment.kt
+++ app/src/main/java/com/plusmobileapps/bottomnavtransition/ui/DashboardFragment.kt
@@ -5,12 +5,18 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.transition.TransitionInflater
+import com.bumptech.glide.Glide
 import com.plusmobileapps.bottomnavtransition.R
 
+const val ANDROID_JETPACK_IMAGE_URL = "https://4.bp.blogspot.com/-NnAkV5vpYuw/XNMYF4RtLvI/AAAAAAAAI70/kdgLm3cnTO4FB4rUC0v9smscN3zHJPlLgCLcBGAs/s1600/Jetpack_logo%2B%25282%2529.png"
+
 class DashboardFragment : Fragment() {
+
+    private lateinit var imageView: ImageView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,10 +30,15 @@ class DashboardFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         val root = inflater.inflate(R.layout.fragment_dashboard, container, false)
-        val textView: TextView = root.findViewById(R.id.text_dashboard)
-        textView.text = "This is dashboard"
+        imageView = root.findViewById(R.id.dashboard_imageview)
         root.findViewById<Button>(R.id.button).setOnClickListener {
             onDetailButtonClicked()
+        }
+        root.findViewById<ImageView>(R.id.dashboard_imageview)?.let { imageView ->
+            Glide.with(requireContext())
+                .load(ANDROID_JETPACK_IMAGE_URL)
+                .placeholder(R.drawable.ic_android_robot)
+                .into(imageView)
         }
         return root
     }
@@ -37,9 +48,12 @@ class DashboardFragment : Fragment() {
         fragmentManager?.beginTransaction()?.let { fragmentTransaction ->
             fragmentTransaction.apply {
                 setReorderingAllowed(true)
-                setCustomAnimations(R.anim.slide_in_bottom, R.anim.slide_out_bottom, R.anim.slide_in_bottom, R.anim.slide_out_bottom)
+                addSharedElement(imageView, imageView.transitionName)
                 replace(R.id.container_content, DashboardDetailFragment())
-                bottomNavFragment?.let { remove(it) }
+                bottomNavFragment?.let {
+                    setCustomAnimations(R.anim.slide_in_bottom, R.anim.slide_out_bottom, R.anim.slide_in_bottom, R.anim.slide_out_bottom)
+                    remove(it)
+                }
                 addToBackStack(null)
                 commit()
             }
